8068a85aa8732d596ec97200813c344b70f34702
Pavel Bezborodov <dfa.sro591@gmail.com>
2023-07-04 19:08:12 +0300

    sway: improve lock scripts add sounds

--- sway/scripts/idle_notify.sh
+++ sway/scripts/idle_notify.sh
@@ -3,6 +3,7 @@
 arg=$1
 ttl=$((ttl * 1000))
 notification_id=$(dunstify -p -t $ttl "Screen will be locked soon")
+paplay $(dirname $0)/notify.wav &
 
 trap "dunstify -C $notification_id; exit" SIGINT SIGTERM SIGKILL
 
