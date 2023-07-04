08a8f233754b8f327ba032634b17585c238390aa
Pavel Bezborodov <dfa.sro591@gmail.com>
2023-07-04 19:49:49 +0300

    sway: improve idle script

--- sway/scripts/idle_notify.sh
+++ sway/scripts/idle_notify.sh
@@ -1,12 +1,13 @@
 #!/bin/zsh
 
-arg=$1
-ttl=$((ttl * 1000))
+arg=${1:-5}
+ttl=$((arg * 1000))
+
 notification_id=$(dunstify -p -t $ttl "Screen will be locked soon")
 paplay $(dirname $0)/notify.wav &
 
 trap "dunstify -C $notification_id; exit" SIGINT SIGTERM SIGKILL
 
-while true; do
+for _ in {1..70}; do
     sleep 1
 done
